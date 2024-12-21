a71af49d1d9ef8d218e5e548bc8608656fb39e06
JaydenShofolahan <jaydenshofolahan@gmail.com>
2024-12-21 16:58:59 -0500

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,26 @@
+//Name: Jayden Shofolahan 
+//Date: 12/21/24
+//Description: This will evaluate an age 
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+  int age;
+  cout<<"Enter an age"; 
+  cin>>age; 
+   
+
+  while (age<1) {
+    cout<<"Negative number. Try again";
+    cout<<"\n Enter an age";
+    cin>>age;
+
+  }
+  if ( age > 0) 
+  {
+    cout<<("\nYou entered")<<age;
+  }
+  
+  
+}
